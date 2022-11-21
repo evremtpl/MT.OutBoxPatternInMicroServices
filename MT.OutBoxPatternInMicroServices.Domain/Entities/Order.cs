f37213bda7ff2fd0119b3a0cb4283f0dadfd2947
merve topal <merve.tpl@WINDOWSLIVE.COM>
2022-11-21 20:47:15 +0300

    Add project files.

---
+++ MT.OutBoxPatternInMicroServices.Domain/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.OutBoxPatternInMicroServices.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public int  Ouantity { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MT.OutBoxPatternInMicroServices.Domain/Entities/OrderOutBox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.OutBoxPatternInMicroServices.Domain.Entities
+{
+    public class OrderOutBox
+    {
+        public OrderOutBox()
+        {
+
+        }
+        public DateTime OccureOn  { get; set; }
+        public DateTime? ProcessDate { get; set; }
+        public string @Type  { get; set; }
+        public string Payload { get; set; }
+        public Guid IdempotentToken { get; set; }
+    }
+}
